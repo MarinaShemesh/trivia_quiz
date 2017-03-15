c09f7fe4c48b420fe085e3f1e3cc5c6af01189ad
Yejiel Wahnich <yejielw@hotmail.com>
2017-03-15 13:34:50 +0200

    cleaned controller

--- triviaCtrl.js
+++ triviaCtrl.js
@@ -1,24 +1,7 @@
-app.controller('triviaCtrl', [
-    '$scope',
-    '$http',
-    function($scope, $http) {
-        $scope.myScore = 0;
+app.controller('triviaCtrl', function($scope, triviaFactory) {
 
-        $scope.questions = [ ];
+    $scope.triviaObj = triviaFactory;
+
+})
+
        
-                $scope.grabJson = function() {
-            $http.get('https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple').then(function(data) {
-                 console.log(data.data.results["0"].question);
-                 
-                 console.log($scope.quests);
-                 $scope.quests = data;
-               })
-      };
-
-                        //
-                        // var output = document.getElementById('output');
-                        // output.innerHTML = data.data.results["0"].question;
-                  
-    }
-
-]);
