87819ed760ca1301cdaca115b1cf23a4687c636a
Yejiel Wahnich <yejielw@hotmail.com>
2017-03-15 09:52:35 +0200

    first draft

--- triviaCtrl.js
+++ triviaCtrl.js
@@ -4,31 +4,21 @@ app.controller('triviaCtrl', [
     function($scope, $http) {
         $scope.myScore = 0;
 
-        $scope.questions = [
-            {
-                "category": "Science: Computers",
-                "type": "multiple",
-                "difficulty": "easy",
-                "question": "What does CPU stand for?",
-                "correct_answer": "Central Processing Unit",
-                "incorrect_answers": ["Central Process Unit", "Computer Personal Unit", "Central Processor Unit"]
-            }
-
-        ];
-        $scope.grabJson = function() {
-                    $http.get('https://opentdb.com/api.php?amount=30&category=18&difficulty=medium&type=multiple').then(function(data) {
-                         myQuestion = (data.data.results["0"].question);
-                         console.log(myQuestion);
-                         rightAnswer = (data.data.results["0"].correct_answer);
-                        //  wrongAnswer = (data.data.results["0"].incorrect_answers[0,1,2]);
-                         document.querySelector('.results').innerHTML = myQuestion;
-                         document.querySelector('.right').innerHTML = rightAnswer;
-
+        $scope.questions = [ ];
+       
+                $scope.grabJson = function() {
+            $http.get('https://opentdb.com/api.php?amount=20&category=18&difficulty=easy&type=multiple').then(function(data) {
+                 console.log(data.data.results["0"].question);
+                 
+                 console.log($scope.quests);
+                 $scope.quests = data;
+               })
+      };
 
                         //
                         // var output = document.getElementById('output');
                         // output.innerHTML = data.data.results["0"].question;
-                    })
+                  
     }
-}
+
 ]);
